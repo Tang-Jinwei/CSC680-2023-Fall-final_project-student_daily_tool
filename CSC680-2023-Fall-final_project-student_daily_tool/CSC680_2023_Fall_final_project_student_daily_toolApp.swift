3edc9833b16edd921a6e9dcd3da2ca3ad28a0f22
Tang-Jinwei <jinwei.tang.sf@gmail.com>
2023-10-18 22:22:05 -0700

    Initial Commit

---
+++ CSC680-2023-Fall-final_project-student_daily_tool/CSC680_2023_Fall_final_project_student_daily_toolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSC680_2023_Fall_final_project_student_daily_toolApp.swift
+//  CSC680-2023-Fall-final_project-student_daily_tool
+//
+//  Created by Tang Jinwei on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CSC680_2023_Fall_final_project_student_daily_toolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
